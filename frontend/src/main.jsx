a5e69518893a11b8dff4f584a1ee2d2d5edd14b3
waihinsoe <waihinsoe332004@gmail.com>
2024-09-04 22:44:24 +0630

    init

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+window.global = window;
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
